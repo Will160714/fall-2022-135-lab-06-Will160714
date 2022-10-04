410e41162fc3db8b645be7832e940c24a6d1da7b
Will160714 <95445527+Will160714@users.noreply.github.com>
2022-10-04 15:49:08 -0400

    Testing Task D

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,27 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
-#include "funcs.h"
+#include "caesar.h"
+#include "vigenere.h"
+#include "decrypt.h"
 
 // add your tests here
+TEST_CASE("Task B"){
+    CHECK(encryptCaesar("Way to Go!", 5) == "Bfd yt Lt!");
+    CHECK(encryptCaesar("!", 1) == "!");
+    CHECK(encryptCaesar("Hello, World!", 0) == "Hello, World!");
+    CHECK(encryptCaesar("L",5) == "Q");
+    CHECK(encryptCaesar("W", 5) == "B");
+}
+
+TEST_CASE("Task C"){
+    CHECK(encryptVigenere("", "cake") == "");
+    CHECK(encryptVigenere("Hello, World!", "cake") == "Jevpq, Wyvnd!");
+    CHECK(encryptVigenere("Hello, World!", " ") == "Hello, World!");
+    CHECK(encryptVigenere("Test", "") == "Test");
+    CHECK(encryptVigenere(";", "hi") == ";");
+    CHECK(encryptVigenere("P", "eat") == "T");
+}
+
+TEST_CASE("Task D"){
+    CHECK(decryptCaesar("Bfd yt Lt!", 5) == "Way to Go!");
+}
