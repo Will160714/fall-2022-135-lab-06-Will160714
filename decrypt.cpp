012ef0951f28ddb23feca68b118f9d6d1c8e457f
Will160714 <95445527+Will160714@users.noreply.github.com>
2022-10-06 08:57:33 -0400

    Completed code for Function 2

--- decrypt.cpp
+++ decrypt.cpp
@@ -7,5 +7,40 @@ std::string decryptCaesar(std::string ciphertext, int rshift){
 }
 
 std::string decryptVigenere(std::string ciphertext, std::string keyword){
-    return "";
+    if(!isalpha(keyword[0]))
+        return ciphertext;
+    
+    std::string result = "";
+    int count = 0;
+
+    int number[keyword.length()];
+    for(int x = 0; x < keyword.length(); x++){
+        number[x] = (int)(tolower(keyword[x])) - 97;
+    }
+
+    for(int y = 0; y < ciphertext.length(); y++){
+        if(isalpha(ciphertext[y])){
+            if(isupper(ciphertext[y])){
+                if(ciphertext[y] - number[count] >= 65)
+                    result += ciphertext[y] - number[count];
+                else
+                    result += "Z" - (65 - (ciphertext[y] - number[count]));
+            }
+            else{
+                if(ciphertext[y] - number[count] >= 97)
+                    result += ciphertext[y] - number[count];
+                else
+                    result += "z" - (97 - (ciphertext[y] - number[count]));
+            }
+
+            count ++;
+            if(count == keyword.length())
+                count = 0;
+        }
+
+        else
+            result += ciphertext[y];
+    }
+
+    return result;
 }
