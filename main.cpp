9a80933c35dc48c32fc3722e38847982bee098a8
Will160714 <95445527+Will160714@users.noreply.github.com>
2022-10-09 04:21:15 -0400

    Complete

--- main.cpp
+++ main.cpp
@@ -5,5 +5,9 @@
 
 int main()
 {
+  std::cout << "encryptCaesar('Way to Go!', 5): " << encryptCaesar("Way to Go!", 5) << std::endl;
+  std::cout << "encryptVigenere('Hello, World!', 'cake'): " << encryptVigenere("Hello, World!", "cake") << std::endl;
+  std::cout << "decryptCaesar('Bfd yt Lt!', 5): " << decryptCaesar("Bfd yt Lt!", 5) << std::endl;
+  std::cout << "decryptVigenere('Jevpq, Wyvnd!', 'cake'): " << decryptVigenere("Jevpq, Wyvnd!", "cake") << std::endl;
   return 0;
 }
